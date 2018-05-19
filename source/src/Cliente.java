a20d707e72755270f3ee8cd8f0f6c502b1bf5fbf
grr20175502 <rafael2605@gmail.com>
2018-05-19 06:07:52 -0300

    Inicio do código

---
+++ source/src/Cliente.java
@@ -0,0 +1,22 @@
+public class Cliente {
+
+    private cpf char(11);
+    private rg char(9);
+    private endereco Endereco;
+
+    public insereCliente(){
+
+    }
+
+    public atualizaCliente(){
+
+    }
+
+    public consultaCliente() {
+
+    }
+
+    public excluiCliente() {
+
+    }
+}
