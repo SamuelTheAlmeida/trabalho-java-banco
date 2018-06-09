98a47a6a74f9dc5c0b38bcfd263b54f89fe1847d
Enzo Haguiwara <loki@Enzos-MacBook-Air.local>
2018-06-09 02:14:36 -0300

    WIP - Arruma atributos, atualiza insereCliente

--- source/src/Cliente.java
+++ source/src/Cliente.java
@@ -1,11 +1,19 @@
 public class Cliente {
 
-    private cpf char(11);
-    private rg char(9);
-    private endereco Endereco;
+    private char(11) cpf;
+    private char(9) rg;
+    private String endereco;
 
-    public insereCliente(){
+    public insereCliente(String nome, char(11) cpf, char(9) rg, String endereco, int tipoConta){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
 
+        if(tipoConta == 0){
+          Conta conta = new ContaCorrente();
+        }else{
+          Conta conta = new ContaInvestimento();
+        }
     }
 
     public atualizaCliente(){
